80a4f7d027c3dffb4b8f038ce778e12ba0992743
FarID <farid@bagishev.ru>
2012-08-01 16:06:22 +0600

    production only

--- lib/prowl/exception.rb
+++ lib/prowl/exception.rb
@@ -3,7 +3,7 @@ require 'prowl/rack'
 module ProwlException
   class Railtie < ::Rails::Railtie
     initializer "prowlexception.middleware" do |app|
-      app.middleware.use Rack::ProwlException
+      app.middleware.use Rack::ProwlException if ENV['RAILS_ENV'] == 'production'
     end
   end
 end
